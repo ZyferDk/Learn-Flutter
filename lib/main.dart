d981b12f733558f6af42caa898eef87c939a1a9a
surajidk12 <surajidk12@gmail.com>
2021-11-24 20:33:13 +0700

    images & assets

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,31 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
-      body: const Center(
-        child: Text(
-          'Hello Ninjas!',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.grey,
-            fontFamily: 'IndieFlower',
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const <Widget>[
+              Text(
+                'Hello Ninjas!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                  color: Colors.grey,
+                  fontFamily: 'IndieFlower',
+                ),
+              ),
+              // Image(
+              //   image: NetworkImage(
+              //       'https://images.unsplash.com/photo-1637497520435-cb436ebddc8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80'),
+              // ),
+              Image(
+                image: AssetImage('assets/space-2.jpg'),
+              )
+            ],
           ),
         ),
       ),
